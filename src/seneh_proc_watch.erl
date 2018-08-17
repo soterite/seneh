bb516b232f2196b4d608ec2dbb9d5264e49ac1dd
Krystian Więcek <mroq@malina.chlewik>
2018-08-17 16:01:20 +0200

    seneh_proc_watch reworked

--- src/seneh_proc_watch.erl
+++ src/seneh_proc_watch.erl
@@ -1,6 +1,11 @@
 -module(seneh_proc_watch).
+-vsn("1.2").
 
--export([init/1, patrol/1, terminate/1, check/1]).
+-export([
+         init/1,
+         loop/1,
+         patrol/1 
+        ]).
 
 -define(FREQ, 1000 * 60 * 59).
 -define(PS_CMD, "ps -xo cmd").
@@ -12,30 +17,42 @@
 {"ssh -o TCPKeepAlive=yes -N -R 19723:localhost:22 mroq@s2.mydevil.net &",
  "19723:localhost:22"}]).
 
+-record(state, { eventHandler_pid, level }).
+
 init(EHPid) ->
-    patrol([{eventHandler_pid, EHPid}]).
+    ?MODULE:loop(#state{eventHandler_pid = EHPid, level = running}).
 
-patrol(State) ->
-    EHPid = proplists:get_value(eventHandler_pid, State),
-
-    lists:foreach(fun({Cmd, Tunel}) ->
-                    case check(Tunel) of
-                        nomatch -> os:cmd(Cmd),
-                                   EHPid ! {self(), Tunel ++ " restart"};
-                        _Match  -> ok
-                    end
-                  end, ?S2_TUNEL),
+loop(State) ->
+    EHPid = State#state.eventHandler_pid,
 
     receive
-        {EHPid, stop} -> terminate(State)
+        {EHPid, terminate}  -> NewState = State#state{level = shutdown};
+        {EHPid, check}      -> ?MODULE:patrol(EHPid),
+                               NewState = State;
+        {EHPid, info}       -> EHPid ! {ok, {State, ?MODULE:module_info(attributes)}},
+                               NewState = State
     after ?FREQ ->
-        EHPid ! {self(), alive},
-        patrol(State)
+        ?MODULE:patrol(EHPid),
+        NewState = State
+    end,
+
+    case NewState#state.level of
+         running    -> ?MODULE:loop(NewState);
+         shutdown   -> {EHPid, {ok, terminating}}
     end.
 
-check(Process) ->
-    string:find(os:cmd(?PS_CMD), Process).
+patrol(MSGPid) ->
+    MSGPid ! {self(), {ok, checking_now}},
 
-terminate(State) ->
-    EHPid = proplists:get_value(eventHandler_pid, State),
-    EHPid ! {self(), terminate}.
+    lists:foreach(fun({Cmd, Tunel}) ->
+                    case find_process(Tunel) of
+                        nomatch -> os:cmd(Cmd),
+                                   MSGPid ! {self(), Tunel ++ " restart"};
+                        _Match  -> ok
+                    end
+                  end, ?S2_TUNEL).
+
+%% HELPERS
+%%
+find_process(Process) ->
+    string:find(os:cmd(?PS_CMD), Process).
