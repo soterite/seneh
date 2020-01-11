f61cbab00ef33c9a9c14d3edef46aa1b69bc44c5
Krystian Wiecek <kwmroq@gmail.com>
2020-01-11 21:40:50 +0100

    add vm config

--- src/seneh_sup.erl
+++ src/seneh_sup.erl
@@ -30,21 +30,22 @@ start_link() ->
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
 
-    WatcherChild = #{id     => watcher, % to jest wewnetrzny identyfikator supervisora - Obowiazkowy
-                     start  => {seneh_watch, start, [self()]}, % funkcja startująca childa. Obowiazkowa. Chyba może być (powinno?) to tylko start_link
-                     restart => permanent, % opcjonalnie i defaultowo. Może też być temporary (never restarted)
-                                           % lub transient (restarted gdy exit reason inny niż normal
-                     shutdown => 5000, % defaultowo i opcjonalnie. Najpierw exit(Child, shutdown) a po milisekundach exit(Child kill).
-                                       % inne: brutal_kill (zawsze uzywa exit(Child, kill)) albo
-                                       % infinity (default jeśli type jest supervisor). Tylko exit(Child, shutdown)
-                     type => worker, % defaultowo. Może też być supervisor
-                     modules => [seneh_watch]},
+    %% WatcherChild = #{id     => watcher, % to jest wewnetrzny identyfikator supervisora - Obowiazkowy
+    %%                  start  => {seneh_watch, start, [self()]}, % funkcja startująca childa. Obowiazkowa. Chyba może być (powinno?) to tylko start_link
+    %%                  restart => permanent, % opcjonalnie i defaultowo. Może też być temporary (never restarted)
+    %%                                        % lub transient (restarted gdy exit reason inny niż normal
+    %%                  shutdown => 5000, % defaultowo i opcjonalnie. Najpierw exit(Child, shutdown) a po milisekundach exit(Child kill).
+    %%                                    % inne: brutal_kill (zawsze uzywa exit(Child, kill)) albo
+    %%                                    % infinity (default jeśli type jest supervisor). Tylko exit(Child, shutdown)
+    %%                  type => worker, % defaultowo. Może też być supervisor
+    %%                  modules => [seneh_watch]},
 
     SupFlags = #{strategy => one_for_one,
                  intensity => 1,        % dziiecko moze sie maksymalnie zrestartowac tyle razy (MaxR)
                  period => 1},          % w jednej sekundzie (MaxT).
 
-    {ok, { SupFlags, [WatcherChild]} }.
+    %% {ok, { SupFlags, [WatcherChild]} }.
+    {ok, { SupFlags, []} }.
 
 %%====================================================================
 %% Internal functions
